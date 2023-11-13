529142520deca76967af15870a7482d4489516f7
vitorh07 <133894512+vitorh07@users.noreply.github.com>
2023-11-13 15:13:27 -0300

    Add files via upload

---
+++ PrjLombok/src/main/java/com/vitor/Lombok/PrjLombok/PrjLombokApplication.java
@@ -0,0 +1,13 @@
+package com.vitor.Lombok.PrjLombok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjLombokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjLombokApplication.class, args);
+	}
+
+}
