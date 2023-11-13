b8718ed237daeb477202039e2e4e6994ec5ccb19
vitorh07 <133894512+vitorh07@users.noreply.github.com>
2023-11-13 08:44:16 -0300

    Delete PrjLombok directory

--- PrjLombok/src/main/java/com/vitor/Lombok/PrjLombok/repositories/UsuarioRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.vitor.Lombok.PrjLombok.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.vitor.Lombok.PrjLombok.entities.Usuario;
-
-public interface UsuarioRepositorio extends JpaRepository <Usuario, Long> {
-
-}
